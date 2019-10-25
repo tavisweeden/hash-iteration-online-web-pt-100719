1587df749a1a4311e821891601187a6bca9f9de3
Tavis Weeden <tavisweeden@gmail.com>
2019-10-25 02:04:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,11 +4,11 @@
 #   "Amanda" => 27
 # }
 
-# def happy_birthday(birthday_kids)
-#   birthday_kids.each do |name, age|
-#     puts "Happy Birthday #{name}! You are now #{age} years old!"
-# end
-# end
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
+     puts "Happy Birthday #{name}! You are now #{age} years old!"
+end
+end
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
